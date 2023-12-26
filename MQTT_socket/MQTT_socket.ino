2e93064e75ab5d384c88220d41179acf9d1bc42c
ngochungEE2 <ngaothe73@gmail.com>
2023-12-26 21:38:55 +0700

    Lay IP tu dong

--- MQTT_socket/MQTT_socket.ino
+++ MQTT_socket/MQTT_socket.ino
@@ -1,40 +1,21 @@
 #include <ArduinoJson.h>
 #include <WiFiManager.h>
 #include <PubSubClient.h>
-const char* ssid = "LingLing";
-const char* password = "menhmonghoaha";
-const char* mqtt_server = "192.168.43.25";
-#define Led_Pin 20
-#define Led_State 8
-//const char* mqtt_user = "Hunggg";
-//const char* mqtt_pass = "Lh200819";
+#include <ESPAsyncUDP.h>
+#define TRIGGER_PIN 23
+#define Led_Pin 2
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 WiFiManager wf;
-String nameTag;
-uint8_t period = 3;
-WiFiManagerParameter set_device_name("nameTag", "Device name", "", 30);
+String nameTag = "home_sensor";
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "mqtt-explore";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
-      client.subscribe("evse_service/EVSE45678/Receive");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
+AsyncUDP udp;
+IPAddress centralIP;
+bool serverConnect = false;
+const String cmd = "BUSTER CALL";
+uint8_t period = 2;
+
 // Callback + change period
 void callback_period(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
@@ -56,27 +37,70 @@ void callback_period(char* topic, byte* payload, unsigned int length) {
     digitalWrite(Led_Pin,LOW);
   }
   // Update periodValue with received value
-  if(strcmp(topic,"ProjectIoT/1/period")==0)
+  if (strcmp(topic, "ProjectIoT/1/period") == 0)
   {
-  // Convert payload to integer for period value
-  String payloadStr = "";
-  for (int i = 0; i < length; i++) {
-    payloadStr += (char)payload[i];
-  }
-  period = payloadStr.toInt();
+    // Convert payload to integer for period value
+    String payloadStr = "";
+    for (int i = 0; i < length; i++) {
+      payloadStr += (char)payload[i];
+    }
+    period = payloadStr.toInt();
   }
 }
 
+void getIPHomeCenter() {
+  Serial.println("Connect to Home Center");
+  String msg = nameTag + "/1";
+  udp.broadcast(msg.c_str());
+}
 void setup() {
-  Serial.begin(9600);
-  pinMode(Led_Pin,OUTPUT);
-  pinMode(Led_State, INPUT);
-  wf.addParameter(&set_device_name);
-  wf.setSaveParamsCallback(saveParamsCallback);
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
+  Serial.begin(115200);
+  pinMode(TRIGGER_PIN, INPUT_PULLUP);
+  pinMode(Led_Pin, OUTPUT);
+  bool res = wf.autoConnect("TM-NgocHung", "12345678");
+  if (!res) {
+    Serial.println("Failed to connect");
+  } else {
+    Serial.println("connected...yay :)");
+  }
+  if (udp.listen(1234)) {
+    Serial.print("UDP Listening on IP: ");
+    Serial.println(WiFi.localIP());
+    udp.onPacket([](AsyncUDPPacket packet) {
+      Serial.print("UDP Packet Type: ");
+      Serial.print(packet.isBroadcast() ? "Broadcast" : packet.isMulticast() ? "Multicast"
+                   : "Unicast");
+      Serial.print(", From: ");
+      Serial.print(packet.remoteIP());
+      Serial.print(":");
+      Serial.print(packet.remotePort());
+      Serial.print(", To: ");
+      Serial.print(packet.localIP());
+      Serial.print(":");
+      Serial.print(packet.localPort());
+      Serial.print(", Length: ");
+      Serial.print(packet.length());
+      Serial.print(", Data: ");
+      Serial.write(packet.data(), packet.length());
+      Serial.println();
+      if (!packet.isBroadcast() && !packet.isMulticast()) {
+        String a = String((char *)packet.data());
+        if (a.equals(cmd)) {
+          centralIP = packet.remoteIP();
+          serverConnect = true;
+          Serial.println("connected to server");
+          Serial.println(centralIP);
+          udp.close();
+        }
+      }
+    });
+  }
+  client.subscribe("IoT/sensor/1");
+  client.setServer(centralIP, 1883);
   client.setCallback(callback_period);
 }
+
+
 void checkButton() {
   // check for button press
   if (digitalRead(TRIGGER_PIN) == LOW) {
@@ -110,35 +134,9 @@ void checkButton() {
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
+  if (!serverConnect) {
+    getIPHomeCenter();
   }
   client.loop();
   checkButton();
-  String state="";
-  if(digitalRead(Led_State)==LOW)
-  {
-    state=state+"OFF";
-  }
-  else
-  {
-    state=state+"ON";
-  }
-  DynamicJsonDocument doc(200);
-  doc[nameTag]["period"] = period;
-  doc[nameTag]["state"] = state;
-  Serial.print("Send : ");
-  String data;
-  serializeJson(doc, data);
-  Serial.println(data);
-  client.publish("ProjectIoT/1/sensor", data.c_str());
-  int delayValue= period*1000;
-  delay(delayValue);
-}
-void saveParamsCallback() {
-  Serial.println("Get Params:");
-  Serial.print(set_device_name.getID());
-  Serial.print(" : ");
-  Serial.println(set_device_name.getValue());
-  nameTag = String(set_device_name.getValue());
 }
